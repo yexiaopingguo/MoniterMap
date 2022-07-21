c6f939c99ff0a12858e19715a22767ecf03df490
Steve900125 <900125kevin@gmail.com>
2022-07-21 15:59:26 +0800

    初始版面配置

---
+++ js/map.js
@@ -0,0 +1,20 @@
+function init(){
+    startTime();
+    console.log("Init Successfully!");
+}
+function startTime(){
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();// 在小于10的数字前加一个‘0’
+    m=checkTime(m);
+    s=checkTime(s);
+    document.getElementById('time').innerHTML=h+":"+m+":"+s;
+    t=setTimeout(function(){startTime()},500);
+}
+function checkTime(i){
+    if (i<10){
+        i="0" + i;
+    }
+    return i;
+}
